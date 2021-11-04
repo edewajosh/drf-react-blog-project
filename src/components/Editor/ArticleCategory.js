c46ed46db10c8f4c129491a3fe27d725341395ce
edewajosh <joshedewa@gmail.com>
2021-11-04 16:53:12 +0300

    - Added article category form

--- src/components/Editor/ArticleCategory.js
+++ src/components/Editor/ArticleCategory.js
@@ -1,6 +1,21 @@
+import { useForm } from "react-hook-form";
+
 const ArticleCategory = () => {
+    const {register, handleSubmit, formState: {errors}} = useForm()
+
+    const onSubmit = () => {
+        return null
+    }
     return(
-        <div>Add Article category</div>
+        <div>
+            <form onSubmit={handleSubmit(onSubmit)}>
+                <input type="text" name="title" {...register("title", {required: true})} placeholder="Title"/>
+                {errors.title && <p>Title Required</p>}
+                <input type="text" name="description" {...register("description", {required: true})} placeholder="Description"/>
+                {errors.description && <p>Description Required</p>}
+                <input type="submit" value="Submit"/>
+            </form>
+        </div>
     )
 }
 

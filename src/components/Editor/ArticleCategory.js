3ac7970fad1c1365c6701a94400848908698dd6e
edewajosh <joshedewa@gmail.com>
2021-11-05 09:27:19 +0300

    - styled editor

--- src/components/Editor/ArticleCategory.js
+++ src/components/Editor/ArticleCategory.js
@@ -3,17 +3,17 @@ import { useForm } from "react-hook-form";
 const ArticleCategory = () => {
     const {register, handleSubmit, formState: {errors}} = useForm()
 
-    const onSubmit = () => {
-        return null
+    const onSubmit = (data) => {
+        console.log(data)
     }
     return(
-        <div>
-            <form onSubmit={handleSubmit(onSubmit)}>
-                <input type="text" name="title" {...register("title", {required: true})} placeholder="Title"/>
+        <div className={'content'}>
+            <form className={'form'} onSubmit={handleSubmit(onSubmit)}>
+                <input className={"control-form"} type="text" name="title" {...register("title", {required: true})} placeholder="Title"/>
                 {errors.title && <p>Title Required</p>}
-                <input type="text" name="description" {...register("description", {required: true})} placeholder="Description"/>
+                <input className={"control-form"} type="text" name="description" {...register("description", {required: true})} placeholder="Description"/>
                 {errors.description && <p>Description Required</p>}
-                <input type="submit" value="Submit"/>
+                <input type="submit" value="Submit" className={"author-submit"}/>
             </form>
         </div>
     )
